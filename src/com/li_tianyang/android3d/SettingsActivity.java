860c3c1968ea10a0ec8d3c1f10c3538d782d7ebc
Tianyang Li 李天阳 <ty@li-tianyang.com>
2014-03-23 11:25:05 +0800

    %%%

---
+++ src/com/li_tianyang/android3d/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.li_tianyang.android3d;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
